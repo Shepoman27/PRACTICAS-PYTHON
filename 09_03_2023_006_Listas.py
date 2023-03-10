99c01d0f62fff5d9ae2af86164495d4034b9cded
Shepoman27 <127465165+Shepoman27@users.noreply.github.com>
2023-03-09 23:32:41 -0600

    Add files via upload

---
+++ 09_03_2023_006_Listas.py
@@ -0,0 +1,15 @@
+#CAPITULO X Y XI LISTAS Y LISTAS NEGATIVAS
+
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores[3]) #Imprimimos el elemento de la lista que corresponde a la posición 3
+print(colores[0] + colores[7]) #Es la posición que corresponde a los colores rojo y rosa
+colores2 = ["tres", "dos", "cinco", "cuatro", "uno"]
+print(str(colores2[4])+" "+str(colores2[1])+" "+str(colores2[0])+" "+str(colores2[3])+" "+str(colores2[2]))
+#Cresmos otra lista con las posiciones requeridas
+
+
+
+print(str(colores[-1])+"\n" + str(colores[-7])+"\n"+str(colores[-5]))
+print(str(colores[-2])+"\n" + str(colores[-8]))
+
+
